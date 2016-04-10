8cfc65554d15cbfedb5adeb6e473146c00f41828
shuaike <tingshuaike@gmail.com>
2016-04-10 16:15:29 +0800

    HelloWorldNettyServer MsgChannelHandler

---
+++ Tiny-Netty-Server/src/com/tingshuaike/tinynettyserver/Demos/HelloWorldNettyServer.java
@@ -0,0 +1,60 @@
+package com.tingshuaike.tinynettyserver.Demos;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @author kuby 2016.04.10 the first demo for netty server demo
+ */
+public class HelloWorldNettyServer {
+	private int port;
+
+	public HelloWorldNettyServer(int port) {
+		this.port = port;
+	}
+
+	public void run() throws InterruptedException {
+		// the netty server need config two EventLoopGroup but the client need
+		// only one
+		EventLoopGroup bossGroup = new NioEventLoopGroup();
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+		try {
+			ServerBootstrap b = new ServerBootstrap();
+			b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+					.childHandler(new ChannelInitializer<SocketChannel>() {
+						protected void initChannel(SocketChannel ch) throws Exception {
+							ch.pipeline().addLast(new MsgChannelHandler());
+						};
+					}).option(ChannelOption.SO_BACKLOG, 128).childOption(ChannelOption.SO_KEEPALIVE, true);
+			// Bind and start to accept incoming connections
+			ChannelFuture f = b.bind(port).sync();
+
+			// wait until the server socket is closed
+			// shutdown this server
+			f.channel().closeFuture().sync();
+		} finally {
+			workerGroup.shutdownGracefully();
+			bossGroup.shutdownGracefully();
+		}
+	}
+
+	public static void main(String[] args)  throws  Exception {
+		int  port;
+		if(args.length>0)
+		{
+			port=Integer.parseInt(args[0]);
+		}else
+		{
+			port=8080;
+		}
+		new  HelloWorldNettyServer(port).run();
+	}
+}
--- Tiny-Netty-Server/src/com/tingshuaike/tinynettyserver/Demos/MsgChannelHandler.java
+++ Tiny-Netty-Server/src/com/tingshuaike/tinynettyserver/Demos/MsgChannelHandler.java
@@ -1,7 +1,33 @@
 package com.tingshuaike.tinynettyserver.Demos;
 
-import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.ReferenceCountUtil;
 
-public class MsgChannelHandler  extends  ChannelHandlerAdapter  {
+/**
+ * hand the IO Event such as 'socket read' 'new request connect'
+ * 
+ * @author zts 2016.04.10
+ */
+public class MsgChannelHandler extends SimpleChannelInboundHandler {
+	// hand the msg read event
+	@Override
+	protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+		try{
+			String  str;
+			str=new String(((ByteBuf) msg).array());
+			System.out.println("request-"+System.currentTimeMillis()+"-:"+str);
+		}finally
+		{
+			//release  the bytebuf  of  msg
+			ReferenceCountUtil.release(msg);
+		}
+	}
 
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		cause.printStackTrace();
+		ctx.close();
+	}
 }
